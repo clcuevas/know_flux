28e5112655fd93ebaebcb2dafb0aa15bf630a529
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-06-11 10:41:24 -0700

    add dispatcher change

--- js/dispatcher/AppDispatcher.js
+++ js/dispatcher/AppDispatcher.js
@@ -12,5 +12,16 @@
  */
 
 var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
 
-module.exports = new Dispatcher();
+var AppDispatcher = assign({}, Dispatcher.prototype, {
+  //bridge function between the views and the dispatcher
+  handleViewAction: function(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  }
+});
+
+module.exports = AppDispatcher;
